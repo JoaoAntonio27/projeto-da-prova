f300ad29a10f8e4a7ab488080d59cd324a02bbc9
JoaoAntonio27 <126472376+JoaoAntonio27@users.noreply.github.com>
2023-10-18 08:18:48 -0300

    Add files via upload

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,13 @@
+package com.mycompany.mavenproject1;
+import java.util.*;
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        Editora e1 = new Editora("Edt 1", "edt1@email.com");
+        Editora e2 = new Editora("Edt 2", "edt2@email.com");
+        
+        
+        
+        
+    }
+}
